fe1364ed00c773be254a52d9f776ef8d56508e6d
kotathefriends <5070kouta@gmail.com>
2024-05-14 20:16:39 +0900

    Initial Commit

---
+++ LaunchScreenLoop/LaunchScreenLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenLoopApp.swift
+//  LaunchScreenLoop
+//
+//  Created by KOTA TAKAHASHI on 2024/05/14.
+//
+
+import SwiftUI
+
+@main
+struct LaunchScreenLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
